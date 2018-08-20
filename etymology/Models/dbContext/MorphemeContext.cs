c38198caf955614d4b949de695e5e56c36ce35d1
George Kolecsanyi <gyuri.kol@gmail.com>
2018-08-20 13:21:58 +1000

    * etymology.csproj: adding of dbContext for morphemes
    * MorphemeContext.cs:

---
+++ etymology/Models/dbContext/MorphemeContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace etymology.Models.dbContext
+{
+    public class MorphemeContext : DbContext
+    {
+        public MorphemeContext(DbContextOptions<MorphemeContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Morpheme> Morphemes { get; set; }
+    }
+}
